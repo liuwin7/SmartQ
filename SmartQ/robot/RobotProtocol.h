7c01e7ff913b5d922f1e09002af8332e11a67431
liuwin7 <liuwin7@163.com>
2015-11-23 19:15:39 +0800

    add image

--- SmartQ/robot/RobotProtocol.h
+++ SmartQ/robot/RobotProtocol.h
@@ -10,6 +10,9 @@
 
 typedef void(^RobotAnswerBolck)(NSError *error, id answer);
 
+/**
+ *  All Robots must conform this protocol
+ */
 @protocol RobotProtocol <NSObject>
 
 @property(nonatomic, strong)NSString *robotName;
@@ -18,6 +21,13 @@ typedef void(^RobotAnswerBolck)(NSError *error, id answer);
 
 + (instancetype)sharedRobot;
 
+/**
+ *  A delegate method
+ *
+ *  @param rebot       robot entity
+ *  @param question    the question that robot request
+ *  @param answerBlock response block
+ */
 - (void)ask:(id<RobotProtocol>)rebot something:(NSString *)question answerBlock:(RobotAnswerBolck)answerBlock;
 
 @end
