ad019363e59bc218d41ea20eb3ead42cccf39a64
liuwin7 <liuwin7@163.com>
2015-11-26 10:16:11 +0800

    Add liblinphone and a Class LinphoneManager to manipulate libLinphone API

---
+++ SmartQ/linphone/LinphoneManager.h
@@ -0,0 +1,13 @@
+//
+//  LinphoneManager.h
+//  SmartQ
+//
+//  Created by tropsci on 15/11/26.
+//  Copyright © 2015年 topsci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LinphoneManager : NSObject
+
+@end
---
+++ SmartQ/linphone/LinphoneManager.m
@@ -0,0 +1,13 @@
+//
+//  LinphoneManager.m
+//  SmartQ
+//
+//  Created by tropsci on 15/11/26.
+//  Copyright © 2015年 topsci. All rights reserved.
+//
+
+#import "LinphoneManager.h"
+
+@implementation LinphoneManager
+
+@end
